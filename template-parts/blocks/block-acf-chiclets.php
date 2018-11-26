9dfcfe703b6f37b55b1c5544bef8e7a851e0ff64
Joel Goodman <joel@braverymedia.co>
2018-11-25 21:01:05 -0600

    page stuff. ugh.

--- template-parts/blocks/block-acf-chiclets.php
+++ template-parts/blocks/block-acf-chiclets.php
@@ -1,9 +1,10 @@
 <?php
   $section_title = get_field('section_title');
+  $chiclets_rows = get_field('chiclet');
+  $count = count($chiclets_rows);
   if ( have_rows('chiclet') ) : ?>
     <section class="nus-block-chiclet-grid">
-      <h2><?php echo $section_title; ?></h2>
-      <div class="wp-block--inner">
+      <div class="wp-block--inner has-<?php echo $count; ?>">
         <?php while ( have_rows('chiclet') ) : the_row('chiclet');
           $icon     = get_sub_field('icons');
           $title    = get_sub_field('chiclet_title');
--- template-parts/blocks/block-acf-download-bar.php
+++ template-parts/blocks/block-acf-download-bar.php
@@ -2,7 +2,7 @@
   $db_type = get_field('db_type');
   $left = get_field('left_side');
   $right = get_field('right_side');
-  $button = $right['db_button'];
+  $button = $right['nus_button'];
 ?>
 <aside class="nus-block-download-bar download-bar--<?php echo $db_type; ?>">
   <header>
@@ -10,15 +10,16 @@
   </header>
   <div class="download-bar--action">
     <?php if ( $button ) { ?>
-      <a href="#" class="wp-block-button__link has-background">Download Button</a>
-
+      <div class="wp-block-button">
+        <a href="<?php echo $button['button_url']; ?>" class="wp-block-button__link has-background" style="background-color: <?php echo $button['button_color']; ?>;"><?php echo $button['button_label']; ?></a>
+      </div>
     <?php } ?>
-    <?php while ( have_rows( $right['db_images'] ) ) : the_row( $right['db_images'] ) :
+    <?php if ( have_rows( $right['db_images'] ) ) : while ( have_rows( $right['db_images'] ) ) : the_row( $right['db_images'] );
       $image = get_sub_field('db_image'); ?>
       ?>
       <a href="<?php the_sub_field('db_link'); ?>">
         <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
       </a>
-    <?php endwhile; ?>
+    <?php endwhile;  endif; ?>
   </div>
 </aside>
