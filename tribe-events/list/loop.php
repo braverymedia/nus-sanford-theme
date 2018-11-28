043b75d9328e5e0af63a259fa0cbf670627f001e
Joel Goodman <joel@braverymedia.co>
2018-11-28 09:57:25 -0600

    edit nav links to use our arrows.

--- tribe-events/list/loop.php
+++ tribe-events/list/loop.php
@@ -27,8 +27,25 @@ $more = false;
 
 		<!-- Event  -->
 		<?php
-			// Moved to template parts so we can load more in later
-			get_template_part('template-parts/content/content-list-event'); ?>
+		$post_parent = '';
+		if ( $post->post_parent ) {
+		  $post_parent = ' data-parent-post-id="' . absint( $post->post_parent ) . '"';
+		}
+		?>
+		<div id="post-<?php the_ID() ?>" class="<?php tribe_events_event_classes() ?>" <?php echo $post_parent; ?>>
+		  <?php
+		  $event_type = tribe( 'tec.featured_events' )->is_featured( $post->ID ) ? 'featured' : 'event';
+
+		  /**
+		   * Filters the event type used when selecting a template to render
+		   *
+		   * @param $event_type
+		   */
+		  $event_type = apply_filters( 'tribe_events_list_view_event_type', $event_type );
+
+		  tribe_get_template_part( 'list/single', $event_type );
+		  ?>
+		</div>
 
 
 		<?php do_action( 'tribe_events_inside_after_loop' ); ?>
